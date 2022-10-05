c54b67673034edbf841d9c011fffebf94c48e701
Konstantinos Daras <kd76@liverpool.ac.uk>
2022-10-05 21:30:08 +0100

    Demo

--- demo.js
+++ demo.js
@@ -1,9 +1,12 @@
 var map = L.map('map').setView([39.9897471840457, -75.13893127441406], 11)
 
 // Add basemap
-L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
-  maxZoom: 18,
-  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}{r}.{ext}', {
+	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	subdomains: 'abcd',
+	minZoom: 0,
+	maxZoom: 20,
+	ext: 'png'
 }).addTo(map)
 
 // Add GeoJSON
