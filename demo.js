f34048c15f74db708e64a692494ce76f10f1b7c8
Konstantinos Daras <kd76@liverpool.ac.uk>
2022-10-06 21:24:38 +0100

    SAMHI - First Attempt

--- demo.js
+++ demo.js
@@ -1,7 +1,7 @@
-var map = L.map('map').setView([39.9897471840457, -75.13893127441406], 11)
+var map = L.map('map').setView([52.735044,-1.420020], 7)
 
 // Add basemap
-var layer_OSM = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}{r}.{ext}', {
+var layer_OSM = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.{ext}', {
 	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 	subdomains: 'abcd',
 	minZoom: 0,
@@ -9,24 +9,41 @@ var layer_OSM = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}
 	ext: 'png'
 }).addTo(map)
 
+map.createPane('labels');
+map.getPane('labels').style.zIndex = 550;
+map.getPane('labels').style.pointerEvents = 'none';
+
+var stamenLayer = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-labels/{z}/{x}/{y}{r}.{ext}', {
+	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	subdomains: 'abcd',
+	minZoom: 0,
+	maxZoom: 20,
+	ext: 'png',
+  pane: 'labels'
+}).addTo(map)
+
+map.spin(true);
 // Add GeoJSON
-$.getJSON('./crimes_by_district.geojson', function (geojson) {
+$.getJSON('./data/samhi_simple.json', function (geojson) {
   var choroplethLayer = L.choropleth(geojson, {
-    valueProperty: 'incidents',
-    scale: ['blue','yellow', 'red'],
+    valueProperty: 'dec_2019',
+    scale: ['#29B6F6','#FFF9C4', '#FF5722'],
     steps: 10,
     mode: 'q',
     style: {
-      color: '#fff',
-      weight: 1,
-      fillOpacity: 0.6
+      color: '#D5D8DC',
+      weight: 0.1,
+      fillOpacity: 1.0
     },
     onEachFeature: function (feature, layer) {
-      layer.bindPopup('District ' + feature.properties.dist_num + '<br>' +
-          feature.properties.incidents.toLocaleString() + ' incidents')
+      layer.bindPopup(' SAMHI Index 2019' + '<br>' +
+                      '<hr>' +
+                      'Decile: ' + feature.properties.dec_2019 + '<br>' +
+                      'Z score:' + feature.properties.index_2019.toLocaleString())
     }
   }).addTo(map)
-  
+  map.spin(false);
+    
   // Add legend (don't forget to add the CSS from index.html)
   var legend = L.control({ position: 'bottomright' })
   legend.onAdd = function (map) {
@@ -36,8 +53,9 @@ $.getJSON('./crimes_by_district.geojson', function (geojson) {
     var labels = []
 
     // Add min & max
-    div.innerHTML = '<div class="labels"><div class="min">' + limits[0] + '</div> \
-			<div class="max">' + limits[limits.length - 1] + '</div></div>'
+    div.innerHTML = '<div class="title"> Legend </div>'
+    div.innerHTML = '<div class="labels"><div class="min">' + limits[0] +': Best' + '</div> \
+			<div class="max">' + 'Worst :'+limits[limits.length - 1] + '</div></div>'
 
     limits.forEach(function (limit, index) {
       labels.push('<li style="background-color: ' + colors[index] + '"></li>')
@@ -50,12 +68,17 @@ $.getJSON('./crimes_by_district.geojson', function (geojson) {
 
   var layers = {
     'OpenStreetMap': layer_OSM,
-    'Test': choroplethLayer
+    'SAMHI Index': choroplethLayer,
+    'OSM Labels': stamenLayer
 };
 
 var layersControl = L.control.layers({},
     layers,
     { collapsed: false }).addTo(map);
 })
+
+
+
+
 
 
